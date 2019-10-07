bab9acf42938b80203839c21497d1a97b9b2474e
Carter Rink <carter.m.rink@gmail.com>
2019-10-07 18:51:04 -0400

    gatsby images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require(`path`)
+
 module.exports = {
   plugins: [
     `gatsby-plugin-netlify-cms`,
@@ -7,7 +9,14 @@ module.exports = {
         path: `${__dirname}/blog`,
         name: 'markdown-pages',
       },
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `images`,
+          path: path.join(__dirname, `src`, `images`),
+        },
     },
     `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`, 
+    `gatsby-plugin-sharp`
   ],
 }
